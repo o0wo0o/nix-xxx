b97b021636c5b5f19e58ac2e2c5188acb76bdee6
System administrator <oO.o0w0o.Oo@proton.me>
2026-07-06 14:41:23 +0400

    2

--- configuration.nix
+++ configuration.nix
@@ -5,11 +5,11 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      <home-manager/nixos>
-    ];
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    <home-manager/nixos>
+  ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -42,8 +42,11 @@
   users.users."owo" = {
     isNormalUser = true;
     description = "owo";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [];
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    packages = with pkgs; [ ];
     shell = pkgs.zsh;
   };
 
@@ -60,224 +63,314 @@
     ${config.nix.package}/bin/nix-env -p /nix/var/nix/profiles/system --delete-generations +3
   '';
 
-
   # List packages installed in system profile.
   environment.systemPackages = with pkgs; [
     vim
     jetbrains.pycharm-oss
     wget
     firefox
-    ptyxis
     python3
     tor-browser
     keepass
     telegram-desktop
     amneziawg-tools
     amneziawg-go
-    git  
-    
+    git
+    wl-clipboard
   ];
 
   environment.sessionVariables = {
-    GDK_BACKEND = "wayland";  
+    GDK_BACKEND = "wayland";
     GSK_RENDERER = "ngl";
+  };
+
+  environment.variables = {
+    EDITOR = "hx";
+    SUDO_EDITOR = "hx";
   };
 
   services.tor = {
     enable = true;
     client.enable = true;
 
-
     settings = {
       SocksPort = 1488;
-     
+
     };
   };
 
   system.stateVersion = "26.05"; # Did you read the comment?
 
   # home manager
-  home-manager.users = let
-    configTemplate =  { pkgs, lib, ... }: {
+  home-manager.users =
+    let
+      configTemplate = { pkgs, lib, ... }: {
 
-    home.stateVersion = "26.05"; 
-    
-    home.packages = with pkgs; [
-      gruvbox-gtk-theme
-      gruvbox-plus-icons
-      capitaine-cursors
-      gnomeExtensions.forge
-    ];
+        home.stateVersion = "26.05";
 
-    programs.home-manager.enable = true;
-    fonts.fontconfig.enable = true;     
- 
-    gtk = {
-      enable = true;
-      
-      theme = {
-        name = "Gruvbox-Dark-B";
-        package = pkgs.gruvbox-gtk-theme;
-      };
+        home.packages = with pkgs; [
+          gruvbox-gtk-theme
+          gruvbox-plus-icons
+          capitaine-cursors
 
-      iconTheme = {
-        name = "Gruvbox-Plus-Dark";
-        package = pkgs.gruvbox-plus-icons;
-      };
-      
-      cursorTheme = {
-        name = "capitaine-cursors";
-        package = pkgs.capitaine-cursors;
-      };
-
-      gtk3.extraConfig = {
-        gtk-application-prefer-dark-theme = 1;
-      };
-
-      gtk4.extraConfig = {
-        gtk-application-prefer-dark-theme = 1;
-      };
-    };
-    
-    programs.zsh = {
-      enable = true;
-      enableCompletion = true;
-      autosuggestion.enable = true;
-      syntaxHighlighting.enable = true;
-
-      history = {
-        size = 50000;
-        save = 50000;
-        ignoreDups = true;
-      };
-    };
-
-    programs.starship = {
-      enable = true;
-      enableZshIntegration = true;
-      
-      settings = builtins.fromTOML (builtins.readFile (pkgs.fetchurl {
-        url = "https://raw.githubusercontent.com/fang2hou/starship-gruvbox-rainbow/refs/heads/main/starship.toml";
-        sha256 = "sha256-DEQN4VZjMQevnAsGI4EE+UJH3B+948PYuUzhCMQfXsw=";
-      }));
-    };
-
-    programs.helix = {
-      enable = true;
-  
-      settings = {
-        theme = "gruvbox_transparent"; 
-    
-        editor = {
-          line-number = "relative";
-          mouse = true;
-          lsp.display-messages = true;
-          auto-completion = true;
-        };
-    
-        editor.cursor-shape = {
-          normal = "block";
-          insert = "bar";
-          select = "underline";
-        };
-      };
-
-      languages.language-server = {
-        pyright = { command = "pyright-langserver"; args = [ "--stdio" ]; };
-        nixd = { command = "nixd"; };
-        vtsls = { command = "vtsls"; args = [ "--stdio" ]; };
-        sqls = { command = "sqls"; };
-        phpactor = { command = "phpactor"; args = [ "language-server" ]; };
-      };
-
-      languages.language = [
-        {
-          name = "nix";
-          language-servers = [ "nixd" ];
-          formatter.command = lib.getExe pkgs.nixfmt-rfc-style;
-          auto-format = true;
-        }
-        {
-          name = "python";
-          language-servers = [ "pyright" ];
-          formatter = { command = lib.getExe pkgs.black; args = [ "-" "--quiet" ]; };
-          auto-format = true;
-        }
-        {
-          name = "javascript";
-          language-servers = [ "vtsls" ];
-          formatter = { command = lib.getExe pkgs.prettier; args = [ "--parser" "typescript" ]; };
-          auto-format = true;
-        }
-       {
-          name = "php";
-          language-servers = [ "phpactor" ];
-          auto-format = false;
-        }
-        {
-          name = "sql";
-          language-servers = [ "sqls" ];
-        }
-      ];
-
-      themes = {
-        gruvbox_transparent = {
-          "inherits" = "gruvbox";
-          "ui.background" = { };
-        };
-      };
-
-      extraPackages = with pkgs; [
-        pyright
-        black
-        vtsls
-        prettier
-        sqls
-        phpactor
-        nixd
-        nixfmt-rfc-style
-      ];
-    };
-
-    dconf.settings = {
-      "org/gnome/Ptyxis" = {
-        theme-set-by-appearance = true;
-        font-name = "JetBrainsMono Nerd Font 11";
-        use-system-font = false;
-        default-profile-uuid = "4ef800c438c0bbb23deced356a4536bb";
-      };
-
-      "org/gnome/Ptyxis/Profiles/4ef800c438c0bbb23deced356a4536bb" = {
-        opacity = 0.80;
-      };
-     
-      "org/gnome/Ptyxis/Profiles/default" = {
-        audible-bell = false;
-        scrollback-lines = 10000;
-      }; 
-
-      "org/gnome/shell/extensions/user-theme" = {
-        name = "Gruvbox-Dark-B";
-      };
-
-      "org/gnome/desktop/interface" = {
-        color-scheme = "prefer-dark";
-      };
-     
-      "org/gnome/desktop/input-sources" = {
-        sources = [
-          (lib.gvariant.mkTuple [ "xkb" "us" ])
-          (lib.gvariant.mkTuple [ "xkb" "ru" ])
         ];
-        xkb-options = [ "grp:alt_shift_toggle" ];
+
+        programs.home-manager.enable = true;
+        fonts.fontconfig.enable = true;
+
+        gtk = {
+          enable = true;
+
+          theme = {
+            name = "Gruvbox-Dark-B";
+            package = pkgs.gruvbox-gtk-theme;
+          };
+
+          iconTheme = {
+            name = "Gruvbox-Plus-Dark";
+            package = pkgs.gruvbox-plus-icons;
+          };
+
+          cursorTheme = {
+            name = "capitaine-cursors";
+            package = pkgs.capitaine-cursors;
+          };
+
+          gtk3.extraConfig = {
+            gtk-application-prefer-dark-theme = 1;
+          };
+
+          gtk4.extraConfig = {
+            gtk-application-prefer-dark-theme = 1;
+          };
+        };
+
+        programs.zsh = {
+          enable = true;
+          enableCompletion = true;
+          autosuggestion.enable = true;
+          syntaxHighlighting.enable = true;
+
+          initExtra = ''
+            # Автозапуск Zellij только внутри Foot с замещением процесса Zsh
+            if [[ "$TERMINAL_EMULATOR" == "foot" || "$TERM" == "foot" ]] && [ -z "$ZELLIJ" ]; then
+                exec zellij attach -c
+            fi
+          '';
+
+          history = {
+            size = 50000;
+            save = 50000;
+            ignoreDups = true;
+          };
+        };
+
+        programs.starship = {
+          enable = true;
+          enableZshIntegration = true;
+
+          settings =
+            (builtins.fromTOML (
+              builtins.readFile (
+                pkgs.fetchurl {
+                  url = "https://raw.githubusercontent.com/fang2hou/starship-gruvbox-rainbow/refs/heads/main/starship.toml";
+                  sha256 = "sha256-DEQN4VZjMQevnAsGI4EE+UJH3B+948PYuUzhCMQfXsw=";
+                }
+              )
+            ))
+            // {
+              time.disabled = true;
+            };
+        };
+
+        programs.foot = {
+          enable = true;
+
+          settings = {
+            main = {
+              font = "JetBrainsMono Nerd Font:size=13";
+            };
+
+            csd = {
+              preferred = "none";
+              size = 0;
+            };
+
+            colors-dark = {
+              alpha = 0.80;
+
+              # Точное попадание в палитру Helix Gruvbox
+              background = "282828";
+              foreground = "ebdbb2";
+
+              # Базовая палитра (Regular)
+              regular0 = "282828"; # black
+              regular1 = "cc241d"; # red
+              regular2 = "b8bb26"; # green (взят яркий, чтобы ls соответствовал коду)
+              regular3 = "d79921"; # yellow
+              regular4 = "458588"; # оригинальный приглушенный синий (для баланса системы)
+              regular5 = "b16286"; # magenta
+              regular6 = "689d6a"; # cyan (мягкий морской)
+              regular7 = "a89984"; # white
+
+              # Интенсивная палитра (Bright — то, что Helix берет для подсветки кода)
+              bright0 = "928374"; # bright black (комментарии)
+              bright1 = "fb4934"; # bright red
+              bright2 = "b8bb26"; # bright green
+              bright3 = "fabd2f"; # bright yellow
+              bright4 = "00a1a1"; # ВАШ яркий синий/бирюзовый (Helix подсветит им переменные)
+              bright5 = "d3869b"; # bright magenta
+              bright6 = "8ec07c"; # bright cyan
+              bright7 = "fbf1c7"; # bright white
+            };
+          };
+        };
+
+        programs.helix = {
+          enable = true;
+
+          settings = {
+            theme = "gruvbox_transparent";
+
+            editor = {
+              line-number = "relative";
+              clipboard-provider = "wayland";
+              mouse = true;
+
+              lsp.display-messages = true;
+              auto-completion = true;
+            };
+
+            editor.cursor-shape = {
+              normal = "block";
+              insert = "bar";
+              select = "underline";
+            };
+          };
+
+          languages.language-server = {
+            pyright = {
+              command = "pyright-langserver";
+              args = [ "--stdio" ];
+            };
+            nixd = {
+              command = "nixd";
+            };
+            vtsls = {
+              command = "vtsls";
+              args = [ "--stdio" ];
+            };
+            sqls = {
+              command = "sqls";
+            };
+            phpactor = {
+              command = "phpactor";
+              args = [ "language-server" ];
+            };
+          };
+
+          languages.language = [
+            {
+              name = "nix";
+              language-servers = [ "nixd" ];
+              formatter.command = lib.getExe pkgs.nixfmt-rfc-style;
+              auto-format = true;
+            }
+            {
+              name = "python";
+              language-servers = [ "pyright" ];
+              formatter = {
+                command = lib.getExe pkgs.black;
+                args = [
+                  "-"
+                  "--quiet"
+                ];
+              };
+              auto-format = true;
+            }
+            {
+              name = "javascript";
+              language-servers = [ "vtsls" ];
+              formatter = {
+                command = lib.getExe pkgs.prettier;
+                args = [
+                  "--parser"
+                  "typescript"
+                ];
+              };
+              auto-format = true;
+            }
+            {
+              name = "php";
+              language-servers = [ "phpactor" ];
+              auto-format = false;
+            }
+            {
+              name = "sql";
+              language-servers = [ "sqls" ];
+            }
+          ];
+
+          themes = {
+            gruvbox_transparent = {
+              "inherits" = "gruvbox";
+              "ui.background" = { };
+            };
+          };
+
+          extraPackages = with pkgs; [
+            pyright
+            black
+            vtsls
+            prettier
+            sqls
+            phpactor
+            nixd
+            nixfmt-rfc-style
+          ];
+        };
+
+        programs.zellij = {
+          enable = true;
+          enableZshIntegration = false;
+
+          settings = {
+            theme = "gruvbox-dark";
+            copy_command = "wl-copy";
+            copy_on_select = true;
+          };
+        };
+
+        dconf.settings = {
+
+          "org/gnome/shell/extensions/user-theme" = {
+            name = "Gruvbox-Dark-B";
+          };
+
+          "org/gnome/desktop/interface" = {
+            color-scheme = "prefer-dark";
+          };
+
+          "org/gnome/desktop/input-sources" = {
+            sources = [
+              (lib.gvariant.mkTuple [
+                "xkb"
+                "us"
+              ])
+              (lib.gvariant.mkTuple [
+                "xkb"
+                "ru"
+              ])
+            ];
+            xkb-options = [ "grp:alt_shift_toggle" ];
+          };
+        };
       };
+    in
+    builtins.mapAttrs (name: value: configTemplate) {
+      owo = { };
+      root = { };
     };
-  };
-  in 
-  builtins.mapAttrs (name: value: configTemplate) {
-    owo = {};
-    root = {};
-  };
 
   # nix linker
   programs.nix-ld.enable = true;
@@ -289,26 +382,23 @@
     nerd-fonts.jetbrains-mono
     nerd-fonts.symbols-only
   ];
-  
 
   # gnome
   services.xserver = {
-     enable = true;
-     displayManager.gdm.enable = true;
-     desktopManager.gnome.enable = true;
-     
-     xkb = {
-        layout = "us,ru";
-        options = "grp:alt_shift_toggle";
-     };  
-   };
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
 
-xdg.portal = {
-  enable = true;
-  extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-  config.common.default = "*";
-};
+    xkb = {
+      layout = "us,ru";
+      options = "grp:alt_shift_toggle";
+    };
+  };
 
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    config.common.default = "*";
+  };
 
 }
-
